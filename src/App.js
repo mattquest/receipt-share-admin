5a8cb0dfc6f439036ae1bccc89a62154ef8d62d1
Matt Rademacher <mrademacher@gmail.com>
2020-05-25 11:05:48 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import React, { Component } from "react"
+import "./App.css"
+import { withAuthenticator } from "aws-amplify-react"
+import Amplify from "aws-amplify"
+import aws_exports from "./aws-exports"
+import { Container } from "@material-ui/core"
+import MaterialTable from "material-table"
+Amplify.configure(aws_exports)
+
+const data = [{ item: 3 }]
+function setData() {}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App App-body">
+        <Container className="App-table">
+          <MaterialTable
+            editable={{
+              isEditable: (rowData) => rowData.name === "a", // only name(a) rows would be editable
+              isDeletable: (rowData) => rowData.name === "b", // only name(b) rows would be deletable
+              onRowAdd: (newData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    /* setData([...data, newData]); */
+
+                    resolve()
+                  }, 1000)
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    const dataUpdate = [...data]
+                    const index = oldData.tableData.id
+                    dataUpdate[index] = newData
+                    setData([...dataUpdate])
+
+                    resolve()
+                  }, 1000)
+                }),
+              onRowDelete: (oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    const dataDelete = [...data]
+                    const index = oldData.tableData.id
+                    dataDelete.splice(index, 1)
+                    setData([...dataDelete])
+
+                    resolve()
+                  }, 1000)
+                }),
+            }}
+          />
+        </Container>
       </div>
-    );
+    )
   }
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, true)
